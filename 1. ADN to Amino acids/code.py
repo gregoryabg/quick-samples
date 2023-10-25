b0a12630041c35322308c700dbe30229a43fb360
Humberto Barrantes <sudohumberto@gmail.com>
2023-10-25 00:28:11 -0600

    Pydoc

--- 1. ADN to Amino acids/code.py
+++ 1. ADN to Amino acids/code.py
@@ -1,17 +1,37 @@
+"""
+DNA to Protein Translation Tool:
+Takes a DNA sequence and translates it into the corresponding 
+amino acid sequence using the genetic code table.
 
-
-"""1 entrada de cadena ADN (txt)
+Steps:
+1 entrada de cadena ADN (txt)
 2 lectura de la cadena
 3 traductor de adn a aminoacido(bases)
 4 traduccion a aminoacidos
-5 impresion de la cadena de aminoacidos"""
+5 impresion de la cadena de aminoacidos
+
+"""
 
 
+def adn_to_aminoacids(adn_strand):
+    """Converts a DNA strand of any size into a amino acids string
 
-def adn_to_aminoacids(cadena_adn):
-    """" recibe un txt y lo guarda como cadena de ADN"""
-    return cadena_adn
+    Parameters:
+        adn_strand (str) : DNA strand to be converted
+    
+    Returns:
+        str : amino acids string
+    """
+    return adn_strand
+
 
 def codon_to_aminoacid(codon):
-    """tabla de codones a amino acidos"""
-    return 
+    """Converts a single codon from the corresponding amino acid
+    
+    Parameters:
+        codon (str) : Three letters codon
+    
+    Returns:
+        str : amino acid name
+    """
+    return ""
