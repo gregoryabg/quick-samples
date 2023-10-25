27692e39bb08bcfc0ce0790185eef52d2a0757fd
gregoryabg <gregoryalfonsobg@gmail.com>
2023-10-25 00:22:36 -0600

    Intrucciones

---
+++ 1. ADN to Amino acids/code.py
@@ -0,0 +1,17 @@
+
+
+"""1 entrada de cadena ADN (txt)
+2 lectura de la cadena
+3 traductor de adn a aminoacido(bases)
+4 traduccion a aminoacidos
+5 impresion de la cadena de aminoacidos"""
+
+
+
+def adn_to_aminoacids(cadena_adn):
+    """" recibe un txt y lo guarda como cadena de ADN"""
+    return cadena_adn
+
+def codon_to_aminoacid(codon):
+    """tabla de codones a amino acidos"""
+    return 
